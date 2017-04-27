554cdd6f7c467297e60bf6310e2ea99ffa602c99
Koichi Shiraishi <zchee.io@gmail.com>
2017-04-27 22:07:27 +0900

    main: merge cli.go file to main.go
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- main.go
+++ main.go
@@ -6,8 +6,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
@@ -48,4 +51,44 @@ func main() {
 func fatal(err error) {
 	fmt.Fprintln(os.Stderr, err)
 	os.Exit(1)
+}
+
+const (
+	exactArgs = iota
+	minArgs
+	maxArgs
+)
+
+func checkArgs(context *cli.Context, expected, checkType int, args ...string) (err error) {
+	cmdName := context.Command.Name
+	switch checkType {
+	case exactArgs:
+		if context.NArg() != expected {
+			err = fmt.Errorf("%s: %q requires exactly <%s> %d argument(s)", os.Args[0], cmdName, strings.Join(args, " "), expected)
+		}
+	case minArgs:
+		if context.NArg() < expected {
+			err = fmt.Errorf("%s: %q requires a minimum of <%s> %d argument(s)", os.Args[0], cmdName, strings.Join(args, " "), expected)
+		}
+	case maxArgs:
+		if context.NArg() > expected {
+			err = fmt.Errorf("%s: %q requires a maximum of <%s> %d argument(s)", os.Args[0], cmdName, strings.Join(args, " "), expected)
+		}
+	}
+
+	if err != nil {
+		fmt.Printf("Incorrect Usage.\n\n")
+		return err
+	}
+	return nil
+}
+
+type fatalWriter struct {
+	cliErrWriter io.Writer
+}
+
+// Write implements io.Writer interface.
+func (f *fatalWriter) Write(b []byte) (n int, err error) {
+	logrus.Error(string(b))
+	return f.cliErrWriter.Write(b)
 }
